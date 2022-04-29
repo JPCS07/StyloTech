3d9e723a64658e1116f9614cdb895e0f1e3b9cdc
JPCS07 <official.jpcs@gmail.com>
2022-04-29 05:35:28 -0700

    First Commit

---
+++ src/components/cloud/Cloud.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './CloudStyles.css'
+
+const Cloud = () => {
+    return (
+        <div className='cloud'>
+            <div className="container">
+                <div className="content">
+                    <h2><span>About</span> Us</h2>
+                    <p>Defeating today’s threats with tomorrow’s tech We are StyloTech Team, Founded in 2021, StyloTech, formerly known as JPCS Consultancy, is a dominating leader in technology, IT consulting, cyber security & Cyber Security Awareness education.</p>
+                {/* <div><button>Sign Up</button></div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cloud
